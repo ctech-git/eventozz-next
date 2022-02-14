70d653893f9bb0b1586b42adcd0b132be1ccbbcc
RodriguesWall <walace.careca87@gmail.com>
2022-02-14 12:30:23 -0300

    event page

--- pages/eventos.js
+++ pages/eventos.js
@@ -3,6 +3,8 @@ import dynamic from 'next/dynamic';
 const Chart = dynamic(import('react-apexcharts'), { ssr: false });
 import PageBanner from '../components/Common/PageBanner';
 import servicesEventozz from '../services/events';
+import { dateLastAccess } from '../utils/strings';
+import { useRouter } from 'next/router';
 
 
 const options = {
@@ -24,6 +26,8 @@ const options = {
   ];
 
 const PriceDetails = ({ events = [] }) => {
+  const router = useRouter()
+
   const [optionsOne, setOptionsOne] = useState(options);
   const [seriesAll, setSeriesAll] = useState(series);
 
@@ -34,9 +38,16 @@ const PriceDetails = ({ events = [] }) => {
     setisMounted(true);
     setDisplay(true);
     setisMounted(false);
-    console.log("============")
-    console.log(events)
+    console.log("----------------");
+    console.log(events);
+    console.log("----------------");
   }, []);
+  function goToEvents(slug) {
+    console.log(slug)
+    // let link = nome.replaceAll(" ", "-");
+    router.push(`/evento/${slug}`);
+  }
+
 
   return (
     <>
@@ -51,37 +62,48 @@ const PriceDetails = ({ events = [] }) => {
             <div className='col-lg-12 col-md-12'>
               <div className='cryptocurrency-top-stories'>
                 <div className='row justify-content-center'>
-                  <div className='col-lg-4 col-md-4'>
-                    <div className='single-blog-post'>
-                      <div className='post-image'>
-                        <a href='blog-details.html' className='d-block'>
-                          <img src='/images/blog/blog1.jpg' alt='image' />
-                        </a>
-                        <div className='tag-list'>
-                          <a href='blog.html'>Filecoin</a>
+                  {events.map((item, index) => {
+                    return (
+
+                      <div className='col-lg-4 col-md-4'>
+                        <div className='single-blog-post' onClick={() => goToEvents(item.slug)}>
+                          <div className='post-image'>
+                            <a className='d-block'>
+                              <img src={item.foto} alt='image' />
+                            </a>
+                            <div className='tag-list'>
+                              <a className={item.active ?
+                                ("open-button") :
+                                ("close-button")}
+                              >{
+                                  item.active ?
+                                    ("Aberto") :
+                                    ("Fechado")}
+                              </a>
+                            </div>
+                          </div>
+                          <div>
+                            <label>{dateLastAccess(item.data_inicio)}</label>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                              viewBox="0 0 24 24">
+                              <path fill-rule="evenodd"
+                                d="M15.494 12.6a.85.85 0 01-.253.51l-5.246 5.14a.89.89 0 01-.847.22.868.868 0 01-.619-.61.847.847 0 01.23-.828l4.624-4.532L8.76 7.968a.847.847 0 01-.23-.829.868.868 0 01.619-.61.89.89 0 01.847.221l5.246 5.14a.847.847 0 01.253.71z">
+                              </path>
+                            </svg>
+                            <label>{dateLastAccess(item.data_fim)}</label>
+                          </div>
+                          <div className='post-content'>
+                            <h3>
+                              <a >
+                                {item.nome_evento}
+                              </a>
+                            </h3>
+                          </div>
                         </div>
                       </div>
-                      <div className='post-content'>
-                        <h3>
-                          <a href='blog-details.html'>
-                            How Filecoin is Up 50% in a Week & Could Take More
-                            Profits
-                          </a>
-                        </h3>
-                        <p>
-                          Borem ipsum dolor sit amet, adhuc iriure dissentias
-                          est in, est ne diam graece tincidunt.
-                        </p>
-                        <div className='author d-flex align-items-center'>
-                          <img src='/images/user/user1.jpg' alt='image' />
-                          <a href='blog.html'>Steven Smith</a>
-                        </div>
-                        <a href='blog-details.html' className='link-btn'>
-                          <i className='bx bx-right-arrow-alt'></i>
-                        </a>
-                      </div>
-                    </div>
-                  </div>
+                    )
+                  })}
+
 
                 </div>
               </div>
@@ -94,15 +116,17 @@ const PriceDetails = ({ events = [] }) => {
 };
 
 export default PriceDetails;
+
 export async function getServerSideProps() {
   const result = await servicesEventozz.getEventzzGeneral();
+  console.log("----------------");
   console.log(result);
+  console.log("----------------");
 
   let response = [];
 
   if (result?.status === 200 && result?.data?.success) {
     response = result?.data?.data;
-
   }
 
   return {
