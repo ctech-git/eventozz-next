6aa2e681dec8760af5d11a6956a04e59bbe30842
Tiago Rocha <tiagoarrocha@gmail.com>
2022-03-20 22:21:26 -0300

    Adicionando seat preview

---
+++ components/seatPreview/SubTitle/index.js
@@ -0,0 +1,31 @@
+
+import { Container } from 'react-bootstrap';
+import { availableVipSeats, availableGoldSeats, availableSilverSeats } from '../../../mock/seatPreview';
+
+export const SubtitleSeatPreview = ({ }) => {
+
+    return (
+        <div className='subtitle-container'>
+                <div className='subtitle-item'>
+                    <i className='item-seat-preview vip subtitle fa fa-chair'></i>
+                    <span>Área Vip</span>
+                </div>
+                <div className='subtitle-item'>
+                    <i className='item-seat-preview gold subtitle fa fa-chair'></i>
+                    <span>Área Ouro</span>
+                </div>
+                <div className='subtitle-item'>
+                    <i className='item-seat-preview silver subtitle fa fa-chair'></i>
+                    <span>Área Prata</span>
+                </div>
+                <div className='subtitle-item'>
+                    <i className='item-seat-preview black subtitle fa fa-wheelchair'></i>
+                    <span>Área PCD</span>
+                </div>
+                <div className='subtitle-item'>
+                    <i className='item-seat-preview reserved subtitle fa fa-circle'></i>
+                    <span>Reservado</span>
+                </div>
+        </div>
+    );
+}
