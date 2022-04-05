fcf455c58b1e70d7f1886cd6bd79d44d57860031
Wall_Rodrigues <walace.careca87@gmail.com>
2022-04-05 10:39:06 -0300

    fixed error

--- pages/_document.js
+++ pages/_document.js
@@ -12,19 +12,9 @@ class MyDocument extends Document {
         <Head>
           <link rel="icon" type="image/png" href="/images/favicon.png"></link>
 
-          <script dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l] = w[l] || [];w[l].push({'gtm.start':
-    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-NJ68JJB')`,
-          }}>
-          </script>
-
 
         </Head>
         <body>
-          <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NJ68JJB"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
           <Main />
           <NextScript />
         </body>
