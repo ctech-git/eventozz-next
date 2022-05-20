fa4e88526ec01a547df4e9e75ee10e7aff9384b5
Tiago Rocha <tiagoarrocha@gmail.com>
2022-05-20 18:36:15 -0300

    Alteração na api

--- services/axios.js
+++ services/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export const Axios = axios.create({
 
   //baseURL: 'https://api-eventozz.herokuapp.com',
-  baseURL: 'https://api-eventozz-dev.herokuapp.com/',
+  baseURL: 'https://api-eventozz-prod.herokuapp.com/',
   // baseURL: 'http://localhost:3333',
   headers: {
     'Content-Type': 'application/json',
